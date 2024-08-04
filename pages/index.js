3371d3529154dfa868dee2c83654106c214cdb60
unknown <sina.ata25@gmail.com>
2024-08-04 14:02:48 +0330

    serive section completed

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 //Internal Import
 import Style from "../styles/index.module.css"
-import {HeroSection} from "../components/ComponentIndex"
+import {HeroSection,Service} from "../components/ComponentIndex"
 
 const Home = () => {
   return (
     <div className={Style.homePage} >
       <HeroSection/>
+      <Service/>
     </div>
   )
 }
