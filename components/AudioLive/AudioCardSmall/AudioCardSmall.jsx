37ddf74933045911ee43cd2276b02d9be106467e
unknown <sina.ata25@gmail.com>
2024-08-26 03:21:27 +0330

    audio card small jsx completed

--- components/AudioLive/AudioCardSmall/AudioCardSmall.jsx
+++ components/AudioLive/AudioCardSmall/AudioCardSmall.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Image from 'next/image';
+import { TbPlayerPlay, TbPlayerPause} from 'react-icons/tb';
+//internal import
+import Style from "./AudioCardSmall.module.css"
+import images from "../../../img"
 
 function AudioCardSmall(props) {
+    const [play, setPlay] = useState(false)
+    const playMusic=()=>{
+        if(!play){
+            setPlay(true);
+        }else{
+            setPlay(false);
+        }
+    }
     return (
-        <div></div>
+        <div className={Style.audioPlayer}>
+            <div className={Style.audioPlayer_box}>
+                <Image src={images.creatorbackground1} alt="music" className={Style.audioPlayer_box_img} />
+                <div className={Style.audioPlayer_box_info}>
+                  <h4>NFT nusic #1442</h4>
+                  <div className={Style.audioPlayer_box_info_box}>
+                    {/*<LikeProfile/>*/}
+                    <div className={Style.audioPlayer_box_info_box_price}>
+                        <p>1.00 ETH</p>
+                    </div>
+                  </div>      
+                </div>
+                <div className={Style.audioPlayer_box_playBtn} onClick={()=>playMusic()}>
+                    {play ? (<TbPlayerPause/>):(<TbPlayerPlay/>)}
+                </div>
+            </div>
+        </div>
     );
 }
 
