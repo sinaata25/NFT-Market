dace12e2ef31aede7bc0f04f04085272bc3b749b
unknown <sina.ata25@gmail.com>
2024-09-03 19:52:23 +0330

    collection index completed

--- collectionPage/collectionProfile/collectionProfile.jsx
+++ collectionPage/collectionProfile/collectionProfile.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import Image from 'next/image'
+import Style from "./collectionProfile.module.css"
+import { TiSocialFacebook,TiSocialLinkedin,TiSocialYoutube,TiSocialInstagram,TiArrowSortedDown,TiSocialTwitter,TiArrowSortedUp} from 'react-icons/ti'
+import images from "../../img"
+
 
 const collectionProfile = () => {
+  const cardArray=[1,2,3,4];
+
   return (
-    <div>collectionProfile</div>
+    <div className={Style.collectionProfile}>
+      <div className={Style.collectionProfile_box}>
+        <div className={Style.collectionProfile_box_left}>
+          <Image src={images.nft_image_1} alt="nft image" className={Style.collectionProfile_box_left_img} />
+          <div className={Style.collectionProfile_box_left_social}>
+            <a href="#">
+              <TiSocialFacebook/>
+            </a>
+            <a href="#">
+              <TiSocialInstagram/>
+            </a>
+            <a href="#">
+              <TiSocialLinkedin/>
+            </a>
+            <a href="#">
+              <TiSocialTwitter/>
+            </a>
+          </div>
+        </div>
+        <div className={Style.collectionProfile_box_middle}>
+          <h1>Awesome NFTs Collection</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- pages/collection.js
+++ pages/collection.js
@@ -5,10 +5,12 @@ import images from "../img"
 import {Banners,collectionProfile} from "../collectionPage/collectionIndex"
 import {Slider,Brand} from "../components/ComponentIndex"
 import Filter from '../components/Filter/Filter'
+
 const collection = () => {
   return (
     <div className={Style.collection}>
       <Banners bannerImage={images.creatorbackground1} />
+      <collectionProfile/>
     </div>
   )
 }
