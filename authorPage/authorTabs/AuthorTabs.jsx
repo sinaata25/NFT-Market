8cd3c2eef7c16f5b3a65d230fe9e20b3cc0886bf
unknown <sina.ata25@gmail.com>
2024-09-07 14:32:41 +0330

    author files created

---
+++ authorPage/authorTabs/AuthorTabs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthorTabs = () => {
+  return (
+    <div>AuthorTabs</div>
+  )
+}
+
+export default AuthorTabs
---
+++ pages/author.js
@@ -0,0 +1,12 @@
+import React,{useState,useEffect} from 'react'
+import Style from "../styles/author.module.css"
+import {Banner,NftCardTwo} from "../collectionPage/collectionIndex"
+import { Brand } from '../components/ComponentIndex'
+
+const author = () => {
+  return (
+    <div>author</div>
+  )
+}
+
+export default author
