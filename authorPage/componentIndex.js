217801dc4e5a6e901c0211778dd66eb20b0701c1
unknown <sina.ata25@gmail.com>
2024-10-01 14:01:48 +0330

    react author completed

---
+++ authorPage/componentIndex.js
@@ -0,0 +1,4 @@
+import AuthorProfileCard from "./authorProfileCard/AuthorProfileCard"
+import AuthorTabs from "./authorTabs/AuthorTabs"
+
+export {AuthorProfileCard,AuthorTabs};
--- pages/author.js
+++ pages/author.js
@@ -1,11 +1,33 @@
 import React,{useState,useEffect} from 'react'
 import Style from "../styles/author.module.css"
-import {Banner,NftCardTwo} from "../collectionPage/collectionIndex"
-import { Brand } from '../components/ComponentIndex'
-
+import {Banners,NftCardTwo} from "../collectionPage/collectionIndex"
+import { Brand,Title } from '../components/ComponentIndex'
+import images from "../img"
+import { AuthorProfileCard,AuthorTabs } from '../authorPage/componentIndex'
 const author = () => {
+
+  const popularArray =[
+    images.user1,
+    images.user2,
+    images.user3,
+    images.user4,
+    images.user5,
+    images.user6,
+    images.user7,
+    images.user8,
+  ]
+  const [collectiables,setCollectiables]=useState(true);
+  const [created,setCreated]=useState(false);
+  const [like,setLike]=useState(false);
+  const [follower,setFollower]=useState(false);
+  const [following,setFollowing]=useState(false);
+
+
   return (
-    <div>author</div>
+    <div className={Style.banner}>
+        <Banners bannerImage={images.creatorbackground1} />
+        <AuthorProfileCard/>
+    </div>
   )
 }
 
